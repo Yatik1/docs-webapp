685345076f52d6667e9f3dd591a558e7db541f7a
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-24 23:39:11 +0530

    ::nav component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
+      <nav className=' absolute top-[5%] w-full py-10 flex justify-center text-xl text-zinc-500 font-extrabold'>Documents.</nav>
        <h1 
         className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-950'
         >Docs.</h1>
