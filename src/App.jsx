5f6d00bc83f9b73e4e8bab075097c213d79fd5e9
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-24 23:34:14 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-700'>
-       App
+    <div className='relative w-full h-screen bg-zinc-800'>
+       <h1 
+        className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-950'
+        >Docs.</h1>
     </div>
   )
 }
