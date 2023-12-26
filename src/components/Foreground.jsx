d91497921bda28d6b9f53fe1f2f48273df74728d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-26 22:56:44 +0530

    data Props

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+      fileSize: '0.9mb',
+      close: true,
+      tag: {
+        isOpen : true,
+        tagTitle: "Download Now",
+        tagColor: "green"
+      }
+    }
+  ]
+
   return (
     <div className="fixed top-0 left-0 z-[4] w-full h-full">
-       <Card />
+       {data?.map((item,index) => (
+        <Card data={item} />
+       ))}
     </div>
   )
 }
