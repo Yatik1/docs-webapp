d4302423c443e9d8769e05af5e6712ab8ca726c3
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-26 23:03:32 +0530

    dynamic Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {FaRegFileAlt} from "react-icons/fa"
 import {LuDownload} from "react-icons/lu"
+import {IoClose} from 'react-icons/io5'
 
 function Card({data}) {
   return (
@@ -11,12 +12,14 @@ function Card({data}) {
           <div className='flex items-center justify-between px-8 py-6 mb-5 '>
             <h5>{data.fileSize}</h5>
             <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center'>
-               <LuDownload size=".7em" color='#fff'/>
+              {data.close ? <IoClose /> :  <LuDownload size=".7em" color='#fff'/>}
             </span>
           </div>
-          <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-             <h3 className='text-sm font-semibold'>Download Now</h3>
-          </div>
+          { data.tag.isOpen && (
+            <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+               <h3 className='text-sm font-semibold'>Download Now</h3>
+            </div>
+          )}
        </div>
     </div>
   )
