fefcaee091b21b96967873cc7337e06949d8a24b
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-26 22:57:02 +0530

    :: dynamic component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {FaRegFileAlt} from "react-icons/fa"
 import {LuDownload} from "react-icons/lu"
 
-function Card() {
+function Card({data}) {
   return (
     <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
        <FaRegFileAlt />
-       <p className='text-sm leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+       <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
        <div className="footer absolute bottom-0 left-0 w-full ">
           <div className='flex items-center justify-between px-8 py-6 mb-5 '>
-            <h5>0.4mb</h5>
+            <h5>{data.fileSize}</h5>
             <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center'>
                <LuDownload size=".7em" color='#fff'/>
             </span>
