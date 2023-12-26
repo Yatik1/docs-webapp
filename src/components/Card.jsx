456aabe41e79b0ecfafa91365320c136b7fffbef
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-26 23:18:20 +0530

    ::Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,22 +5,26 @@ import {IoClose} from 'react-icons/io5'
 
 function Card({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <div className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
        <FaRegFileAlt />
        <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
-       <div className="footer absolute bottom-0 left-0 w-full ">
+        <div className="footer absolute bottom-0 left-0 w-full ">
           <div className='flex items-center justify-between px-8 py-6 mb-5 '>
+          
             <h5>{data.fileSize}</h5>
             <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center'>
               {data.close ? <IoClose /> :  <LuDownload size=".7em" color='#fff'/>}
             </span>
+
           </div>
+
           { data.tag.isOpen && (
-            <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-               <h3 className='text-sm font-semibold'>Download Now</h3>
+            <div className={`tag w-full py-4 ${data.tag.tagColor === 'blue' ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center cursor-pointer`}>
+               <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
             </div>
           )}
-       </div>
+
+        </div>
     </div>
   )
 }
