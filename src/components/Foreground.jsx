8089654568b5f1678591409b5701bdbd7770aae5
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-26 23:18:15 +0530

    ::desc

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -12,12 +12,30 @@ function Foreground() {
         isOpen : true,
         tagTitle: "Download Now",
         tagColor: "green"
+      } 
+    },{
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+      fileSize: '0.9mb',
+      close: true,
+      tag: {
+        isOpen : true,
+        tagTitle: "Download Now",
+        tagColor: "blue"
+      }
+    },{
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+      fileSize: '0.9mb',
+      close: true,
+      tag: {
+        isOpen : true,
+        tagTitle: "Download Now",
+        tagColor: "green"
       }
     }
   ]
 
   return (
-    <div className="fixed top-0 left-0 z-[4] w-full h-full">
+    <div className="fixed top-0 left-0 z-[4] p-5 w-full h-full gap-5 flex flex-wrap" >
        {data?.map((item,index) => (
         <Card data={item} />
        ))}
